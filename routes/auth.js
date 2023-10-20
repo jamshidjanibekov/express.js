438ab7448f56b557e1e8dbb3c28a7c786e8f5c69
jamshid <jamshidjanibekov0@gmail.com>
2023-10-20 20:18:48 +0500

    Dynamic title

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,15 @@ import {Router} from 'express'
 const router = Router()
 
 router.get('/login', (req, res) =>{
-    res.render('login')
+    res.render('login',{
+        title:'Login | Jamshid',
+        isLogin: true,
+    })
 })
 router.get('/register', (req, res) =>{
-    res.render('register')
+    res.render('register', {
+        title:"Register | Jamshid",
+        isRegister:true,
+    })
 })
 export default router
