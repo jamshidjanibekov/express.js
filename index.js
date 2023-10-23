45f953583e3f39ed8389e5e64ba1bfcd13fefa92
jamshid <jamshidjanibekov0@gmail.com>
2023-10-23 21:58:00 +0500

    Mongo DB

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import  express  from "express";
 import {create} from 'express-handlebars'
+import mongoose from "mongoose";
+import * as dotenv from 'dotenv'
+
+// Routes
 import AuthRoutes from "./routes/auth.js";
 import ProductsRoutes from "./routes/products.js";
+
+dotenv.config()
 
 const app = express()
 
@@ -12,14 +18,28 @@ const hbs = create({
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', './views');
+
 app.use(express.urlencoded({extended:true}))
+app.use(express.static('public'))
+app.use(express.json())
+
 
 app.use(AuthRoutes)
 app.use(ProductsRoutes)
 
+const startApp = () =>{
+    try {
+        mongoose.set('strictQuery', false )
+        mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true}, ()=> console.log('Mongo DB connected'))
+
+        const PORT = process.env.PORT || 4100
+        app.listen(PORT, () =>console.log(`Server is running on port: ${PORT}`))
+
+    }catch (error){
+        console.log(error)
+    }
+}
+startApp()
 
 
-
-
-const PORT = process.env.PORT || 4100
-app.listen(PORT, () =>console.log(`Server is running on port: ${PORT}`))
+// mongodb+srv://<username>:<password>@cluster0.dukysnd.mongodb.net/?retryWrites=true&w=majority
