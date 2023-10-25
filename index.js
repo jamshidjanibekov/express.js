b469d267d1e0535c54cfa8e647655864d665852d
jamshid <jamshidjanibekov0@gmail.com>
2023-10-25 20:10:13 +0500

    Model mongoose, User

--- index.js
+++ index.js
@@ -1,45 +1,39 @@
-import  express  from "express";
+import express from 'express'
 import {create} from 'express-handlebars'
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 import * as dotenv from 'dotenv'
 
-// Routes
-import AuthRoutes from "./routes/auth.js";
-import ProductsRoutes from "./routes/products.js";
+// ROUTES
+import AuthRoutes from './routes/auth.js'
+import ProductsRoutes from './routes/products.js'
 
 dotenv.config()
 
 const app = express()
 
-const hbs = create({
-    defaultLayout:'main',
-    extname:'hbs'
-})
+const hbs = create({defaultLayout: 'main', extname: 'hbs'})
+
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
-app.set('views', './views');
+app.set('views', './views')
 
-app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-
 
 app.use(AuthRoutes)
 app.use(ProductsRoutes)
 
-const startApp = () =>{
-    try {
-        mongoose.set('strictQuery', false )
-        mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true}, ()=> console.log('Mongo DB connected'))
+const startApp = () => {
+	try {
+		mongoose.set('strictQuery', false)
+		mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true}, () => console.log('Mongo DB connected'))
 
-        const PORT = process.env.PORT || 4100
-        app.listen(PORT, () =>console.log(`Server is running on port: ${PORT}`))
-
-    }catch (error){
-        console.log(error)
-    }
+		const PORT = process.env.PORT || 4100
+		app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+	} catch (error) {
+		console.log(error)
+	}
 }
+
 startApp()
-
-
-// mongodb+srv://<username>:<password>@cluster0.dukysnd.mongodb.net/?retryWrites=true&w=majority
--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,35 @@
 import {Router} from 'express'
+import User from '../models/User.js'
 const router = Router()
 
-router.get('/login', (req, res) =>{
-    res.render('login',{
-        title:'Login | Jamshid',
-        isLogin: true,
-    })
-})
-router.get('/register', (req, res) =>{
-    res.render('register', {
-        title:"Register | Jamshid",
-        isRegister:true,
-    })
+router.get('/login', (req, res) => {
+	res.render('login', {
+		title: 'Login | Sammi',
+		isLogin: true,
+	})
 })
 
-router.post('/login', (req, res) =>{
-    console.log(req.body)
-    res.redirect('/')
+router.get('/register', (req, res) => {
+	res.render('register', {
+		title: 'Register | Sammi',
+		isRegister: true,
+	})
 })
-router.post('/register', (req, res) =>{
-    console.log(req.body)
-    res.redirect('/')
+
+router.post('/login', (req, res) => {
+	console.log(req.body)
+	res.redirect('/')
+})
+
+router.post('/register', async (req, res) => {
+	const userData = {
+		firstName: req.body.firstname,
+		lastName: req.body.lastname,
+		email: req.body.email,
+		password: req.body.password,
+	}
+	const user = await User.create(userData)
+	res.redirect('/')
 })
 
 export default router
